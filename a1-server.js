288add95e813aaa8971d2958113f1abb47eae19f
A00220384 <jameshopkins1995@gmail.com>
2015-11-02 11:01:11 +0000

    Update a1-server.js

--- a1-server.js
+++ a1-server.js
@@ -21,7 +21,7 @@ var sum;
 // We will use a remote console logging service for debug messages
 // Define the service variable and connnect
 // You must edit <<<change_to_your_repo_name>>> to be your repository name
-var consolere = require('console-remote-client').connect('console.re','80','<<<change_to_your_repo_name>>>');
+var consolere = require('console-remote-client').connect('console.re','80','<<<A00220384>>>');
 
 // Function to handle web browser requests and server responses
 function handleRequest(request, response){
@@ -33,7 +33,13 @@ function handleRequest(request, response){
     // ASSIGNMENT
     // 1. Add a debug message which ouputs the sum of the simple equation above. Values are available in the augend, addend 
     // and sum (global) variables
+    console.log("The output of sum will be 18." );
+     console.log("The operator is incorrect instead of adding the two variable the program instead multiplies the by one another." );
     // 2. Add a debug message which ouputs the current values of the augend addend and sum variables
+    console.log("The current value of augend is 6");
+    console.log("The current value of addend is 3");
+    console.log("The current value of sum is 18");
+    
 }
 
 // Create an instance of a http server
@@ -45,6 +51,8 @@ server.listen(server_port, server_ip_address, function(){
     
     // ASSIGNMENT
     // 3. Add a debug message which ouputs a message indicating the server is started (listening for user requests).
+    console.warn("Server is starting and listening for user requests");
     // 4. Add a debug message which ouputs your server ip address and your server port number. Your server ip address 
     // and your server port number values are available in the server_ip_address and server_port program global variables
+    console.log("server_port is 8080. server_ip_address is 127.0.0.1")
 });
